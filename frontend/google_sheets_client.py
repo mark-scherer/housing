25878ca8f30fb9adadaf3e9320f15376a5bd6c94
Mark Scherer <mark@Marks-MacBook-Pro.local>
2022-11-30 23:14:53 -0800

    added sorting to smart_append

--- frontend/google_sheets_client.py
+++ frontend/google_sheets_client.py
@@ -5,10 +5,11 @@ TODO
 - switch auth from oauth (personal account requiring manual sign in) to SA
 '''
 
-from typing import Optional, List, Any, Dict
+from typing import Optional, List, Any, Dict, NamedTuple
 from os import path
 from pathlib import Path
 import json
+from collections import OrderedDict
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -25,8 +26,34 @@ UPDATE_VALUE_INPUT_OPTION = 'USER_ENTERED'
 SheetData = List[List[Any]]  # Sheet data as a nested list of cells
 SmartSheetData = List[Dict[str, Any]]  # Sheet data as a list of keyed row data.
 
+
+class HeaderData(NamedTuple):
+    header_to_col_num: OrderedDict[str, int]  # Maps santized column names to 0-based col numbers
+    header_row_num: int  # 0-based header row number
+
+
+class GridRange(NamedTuple):
+    '''Google has special GridRange object that's represented like this.'''
+    min_col: int
+    min_row: int
+    max_col: int
+    max_row: int
+
+    def to_google_GridRange(self, sheet_num: int) -> Dict:
+        '''Convert to a google GridRange object.'''
+        return {
+            'sheetId': sheet_num,
+            'startRowIndex': self.min_row,
+            'endRowIndex': self.max_row,
+            'startColumnIndex': self.min_col,
+            'endColumnIndex': self.max_col
+        }
+
+
 class GoogleSheetsClient:
     '''Helper for interacting with google sheets.'''
+
+    MAX_ROWS = 999
 
     def __init__(self, spreadsheet_id: str):
         self.creds = self._get_creds()
@@ -102,8 +129,8 @@ class GoogleSheetsClient:
         request.execute()
 
 
-    def smart_append(self, _values: SmartSheetData) -> None:
-        '''Append rows to sheet, inteligently matching existing table structure.
+    def smart_append(self, _values: SmartSheetData, sort_key: Optional[str] = None, sort_asc: bool = True) -> None:
+        '''Append rows to sheet, inteligently matching existing table structure and optionally resorting after insert.
         
         - Peeks into column names in provided sheet data, then searches existing sheet
             for header row.
@@ -125,41 +152,27 @@ class GoogleSheetsClient:
 
         Args:
             _values: rows to append.
+            sort_key: optional key (from row data) to sort sheet after row appends.
         '''
 
-        def _sanitize_header(input_header: str) -> str:
-            '''helper for sanitizing header value for fair match comparsion.'''
-            return input_header.lower().strip().replace(' ', '_')
-
-        def _is_headers_match(header_1: str, header_2: str) -> bool:
-            '''Helper for determining if two headers are considered a match.'''
-            return _sanitize_header(header_1) == _sanitize_header(header_2)
-        
-        # Find existing header row.
-        possible_headers = list(_values[0].keys())
+        # Find exisiting headers.
         existing_sheet_data = self.get()
-        headers_tl = None
-        for i, existing_row in enumerate(existing_sheet_data):
-            for j, existing_cell in enumerate(existing_row):
-                if any(_is_headers_match(possible_header, existing_cell) for possible_header in possible_headers):
-                    column_letter = chr(ord('A') + j)
-                    headers_tl = f'{column_letter}{i+1}'
-                    break
-            if headers_tl:
-                break  # Need another break out of the outer loop.
-        if not headers_tl:
+        possible_headers = list(_values[0].keys())
+        header_to_col_num, header_row_num = self._find_headers(sheet_data=existing_sheet_data, possible_headers=possible_headers)
+        if not header_row_num:
             raise ValueError(f'Could not find any of these provided headers ({possible_headers}) in existing sheet: {json.dumps(existing_sheet_data)}')
-        headers = existing_sheet_data[i][j:]
-        glog.info(f'Found these headers starting at {headers_tl}: {headers}')
-
+        
         # Create cell values to append.
+        min_col_num = header_to_col_num[next(iter(header_to_col_num))]
+        max_col_num = header_to_col_num[next(reversed(header_to_col_num))]
+        append_width = max_col_num - min_col_num
         append_data = []
         for i, row_to_append in enumerate(_values):
-            row_data = []
+            row_data = [''] * (append_width + 1)
             populated_cells = 0
-            for header in headers:
-                cell_data = row_to_append.get(_sanitize_header(header), '')
-                row_data.append(cell_data)
+            for header, col_num in header_to_col_num.items():
+                cell_data = row_to_append.get(header, '')
+                row_data[col_num-min_col_num] = cell_data
                 if cell_data:
                     populated_cells += 1
             
@@ -170,8 +183,100 @@ class GoogleSheetsClient:
             append_data.append(row_data)
 
         # Append data.
-        self.append(_range=headers_tl, _values=append_data)
+        table_tl = f'{chr(ord("A") + min_col_num)}{header_row_num}'
+        self.append(_range=table_tl, _values=append_data)
 
+        # Resort, if desired.
+        if sort_key:
+            if sort_key not in header_to_col_num:
+                raise ValueError(f'Did not find column to sort by ({sort_key}) in headers: {header_to_col_num.keys()}')
+            sort_col_num = header_to_col_num[sort_key]
+            sort_range = GridRange(
+                min_row=header_row_num,
+                max_row=self.MAX_ROWS,
+                min_col=min_col_num,
+                max_col=max_col_num
+            )
+            self.sort(col_num=sort_col_num, grid_range=sort_range, asc=sort_asc)
+
+
+    def sort(self, col_num: int, grid_range: GridRange, asc: bool = True) -> None:
+        '''Sort specified range by specified column.
+
+        - Will ignore blank cells included in range.
+        
+        Args:
+            col_num: 0-based column number to sort by.
+            grid_range: GridRange object to sort, including header row.
+            asc: sort ascending? Otherwise desc.
+        '''
+
+        sort_order = 'ASCENDING' if asc else 'DESCENDING'
+        sort_request = {
+            'setBasicFilter': {
+                'filter': {
+                    'sortSpecs': {
+                        'dimensionIndex': col_num,
+                        'sortOrder': sort_order,
+                    },
+                    'range': grid_range.to_google_GridRange(0)
+                }
+            }
+        }
+        request_body = {'requests': [sort_request]}
+        request = self.sheet.batchUpdate(
+            spreadsheetId=self.spreadsheet_id,
+            body=request_body
+        )
+        request.execute()
+
+    
+    def _find_headers(self, sheet_data: SheetData, possible_headers: List[str]) -> Optional[HeaderData]:
+        '''Helper for finding headers in sheet data.
+        
+        Args:
+            sheet_data: complete sheet data to search for headers.
+            possible_headers: list of possible (sanitized) headers to look for.
+
+        Return: HeaderData, if found.
+
+        Raises:
+            ValueError: if a header is found twice.
+        '''
+        
+        def _sanitize_header(input_header: str) -> str:
+            '''helper for sanitizing header value for fair match comparsion.'''
+            return input_header.lower().strip().replace(' ', '_')
+
+        def _is_headers_match(header_1: str, header_2: str) -> bool:
+            '''Helper for determining if two headers are considered a match.'''
+            return _sanitize_header(header_1) == _sanitize_header(header_2)
+        
+        result = None
+        header_to_col_num = OrderedDict()
+        header_row_num = None
+        for i, existing_row in enumerate(sheet_data):
+            for j, existing_cell in enumerate(existing_row):
+                for possible_header in possible_headers:
+                    if _is_headers_match(possible_header, existing_cell):
+                        if possible_header in header_to_col_num:
+                            raise ValueError(f'Found header {possible_header} twice: cols {header_to_col_num[possible_header]} & {j}')
+
+                        header_row_num = i
+                        header_to_col_num[possible_header] = j
+                        
+                        # Since found header match, stop trying to match this cell with other headers.
+                        break
+
+            if header_row_num:
+                # Since found header row, stop looking through rows.
+                break
+
+        if header_row_num:
+            result = HeaderData(header_to_col_num=header_to_col_num, header_row_num=header_row_num)
+            glog.info(f'Found these headers headers at row {header_row_num}: {json.dumps(header_to_col_num)}')
+        
+        return result
 
 
     def _get_creds_oauth(self, token_filepath: Optional[str] = DEFAULT_TOKEN_FILEPATH) -> Credentials:
--- frontend/scripts/google_sheets_dev.py
+++ frontend/scripts/google_sheets_dev.py
@@ -4,7 +4,7 @@ import json
 
 import glog
 
-from housing.frontend.google_sheets_client import GoogleSheetsClient
+from housing.frontend.google_sheets_client import GoogleSheetsClient, GridRange
 
 SPREADSHEET_ID = '196O1m_cgE9FIjHZ_gyAzAhER2QRnuTuwnf72iL43Wr4'  # 'housing bot test'
 UPDATE_RANGE = 'B1'
@@ -14,8 +14,14 @@ UPDATE_VALUES = [
 ]
 NEW_ROW = ['dataNA', 'dataNB', 'dataNC']
 SMART_APPEND_DATA = [
-    {'a': 0, 'b': 2, 'c': 3}
+    {'headerA': 6, 'HeaderB': 'z', 'HeaderC': 'y'}
 ]
+TEST_SORT_GRIDRANGE = GridRange(
+    min_col=1,
+    max_col=5,
+    min_row=1,
+    max_row=100
+)
 
 def main():
 
@@ -31,7 +37,8 @@ def main():
     # Update sheet data & refetch
     # client.update(_range=UPDATE_RANGE, _values=UPDATE_VALUES)
     # client.append(_range=UPDATE_RANGE, _values=[NEW_ROW, NEW_ROW])
-    client.smart_append(_values=SMART_APPEND_DATA)
+    client.smart_append(_values=SMART_APPEND_DATA, sort_key='headerA', sort_asc=False)
+    # client.sort(1, grid_range=TEST_SORT_GRIDRANGE, asc=False)
     sheet_data = client.get()
     glog.info(f'Got updated sheet data: {json.dumps(sheet_data)}')
 
